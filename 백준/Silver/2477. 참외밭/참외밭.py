b9b0134edcbb4cb55b6c2b99f204aa72f6f6df95
bbugi <118972599+bbugi@users.noreply.github.com>
2023-01-23 22:04:21 +0900

    [Silver II] Title: 참외밭, Time: 60 ms, Memory: 34080 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -8,28 +8,14 @@ for i in range(6):
     a, b = map(int, input().split())
     a_list.append(a)
     b_list.append(b)
-
-# print(a_list)
-# print(b_list)
-
+    
 while True:
     if a_list[0] == a_list[2] and a_list[1] == a_list[3]:
         small = (b_list[1] * b_list[2])
+        big = (b_list[4] * b_list[5])
         break
     else:
         a_list.rotate(-1)
         b_list.rotate(-1)
-
-# print(a_list)
-# print(b_list)
-
-max1 = []
-for i in range(1,5):
-    if a_list.count(i) == 1:
-        # print(i) # 2, 4
-        max1.append(b_list[a_list.index(i)])
-# print(max1)
-
-big = max1[0] * max1[1]
-
+        
 print((big - small) * k)
