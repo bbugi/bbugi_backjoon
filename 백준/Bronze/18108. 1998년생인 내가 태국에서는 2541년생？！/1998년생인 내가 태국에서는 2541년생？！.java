3133007b859950f99023ff08a21b6705eeab9837
bbugi <118972599+bbugi@users.noreply.github.com>
2023-04-29 18:33:49 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		System.out.println(2541-1998);
+		// 543년 차이남
+
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		
+		System.out.println(A - 543);
+	}
+
+}
