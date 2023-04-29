891859843d207269b2f9f206cb7a442d8810057c
bbugi <118972599+bbugi@users.noreply.github.com>
2023-04-29 17:56:10 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in); // Scanner 객체 생성
+		
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.print(A + B);
+		
+	}
+
+}
