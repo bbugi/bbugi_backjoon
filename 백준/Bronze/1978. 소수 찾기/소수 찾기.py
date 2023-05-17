0a7d538fedefa24eeae62b6a6e2e522a272e9138
Haejeong Choi <118972599+bbugi@users.noreply.github.com>
2023-05-18 00:14:02 +0900

    [Bronze II] Title: 소수 찾기, Time: 48 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+numbers = map(int, input().split())
+
+sosu = 0
+
+for num in numbers:
+    error = 0
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                error += 1
+        if error == 0:
+            sosu += 1
+
+print(sosu)
