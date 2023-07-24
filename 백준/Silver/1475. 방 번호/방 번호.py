0c57697d6024a7b4b121846ab7fe76a1a5b02337
Haejeong Choi <118972599+bbugi@users.noreply.github.com>
2023-07-24 23:40:13 +0900

    [Silver V] Title: 방 번호, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,17 @@
+n = list(input())
+
+numbers = [0] * 10
+
+for i in n :
+    i = int(i)
+    if i == 9 :
+        numbers[6] += 1
+    else:
+        numbers[i] += 1
+
+if numbers[6] % 2 == 0:
+    numbers[6] = numbers[6] // 2
+else:
+    numbers[6] = numbers[6] // 2 + 1
+
+print(max(numbers))
