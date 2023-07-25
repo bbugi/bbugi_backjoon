0c8dd908be4920f2bb4f9966ab3a3b547969ff4d
Haejeong Choi <118972599+bbugi@users.noreply.github.com>
2023-07-25 12:38:32 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,7 @@
+n = int(input())
+
+for i in range(1, 2*n) :
+    if i <= n:
+        print('*' * i + ' ' * 2*(n-i) + '*' * i)
+    else:
+        print('*' *(2*n-i) + ' ' * 2*(i-n) +  '*' * (2*n-i))
