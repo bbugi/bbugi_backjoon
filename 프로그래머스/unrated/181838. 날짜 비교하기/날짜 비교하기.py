9e2e29aba8f74310d63504a9ae6d8ba104615904
Haejeong Choi <118972599+bbugi@users.noreply.github.com>
2023-07-27 23:32:31 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,11 @@
+def solution(date1, date2):
+
+    if date1 == date2:
+        return 0
+    else:
+        for i in range(3) :
+            if date1[i] > date2[i] :
+                return 0
+
+            if date1[i] < date2[i] :
+                return 1
